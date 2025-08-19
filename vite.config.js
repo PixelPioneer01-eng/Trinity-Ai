0b47a58e516ebfb54e28bbf65ff03945f6f4b179
0011-sketch <makineninaveen121@gmail.com>
2025-08-19 15:03:43 +0530

    next commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: process.env.VITE_BASE_PATH || "/Trinity-Ai",
+  build: {
+    outDir: "build", // Change output from 'dist' to 'build'
+  },
 });
